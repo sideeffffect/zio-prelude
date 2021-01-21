942ac31a1edb1bb92d08233fa10f3de47f08d413
Ondra Pelech <ondra.pelech@gmail.com>
2021-01-21 23:09:00 +0100

    Add backwards pipe operator

--- core/shared/src/main/scala/zio/prelude/package.scala
+++ core/shared/src/main/scala/zio/prelude/package.scala
@@ -184,8 +184,22 @@ package object prelude
     /* Ignores the value, if you explicitly want to do so and avoids "Unused value" compiler warnings. */
     def ignore: Unit = ()
 
-    /** Applies function `f` to a value `a`, like `f(a)`, but in flipped order and doesn't need parentheses. Can be chained, like `x |> f |> g`. */
+    /**
+     * Applies function `f` to an argument `a`, like `f(a)`, but in flipped order and doesn't need parentheses.
+     * Can be chained, like `x |> f |> g` = `g(f(x))`.
+     * Works like a pipe in shell.
+     * See `|:` for the opposite way.
+     */
     def |>[B](f: A => B): B = f(a)
+
+    /**
+     * Applies function `f` to an argument `a`, like `f(a)` and doesn't need parentheses.
+     * Can be chained, like `g |: f |: x` = `g(f(x))`.
+     * Works like a pipe in shell, but in right-to-left direction.
+     * See `|>` for the opposite way.
+     * Note, that by virtue of having `:` at the end, the operator is right-associative.
+     */
+    def |:[B](f: A => B): B = f(a)
 
     /** Applies the function `f` to the value `a`, ignores the result, and returns the original value `a`. Practical for debugging, like `x.someMethod.tee(println(_)).someOtherMethod...` . Similar to the `tee` UNIX command. */
     def tap(f: A => Any): A = {
